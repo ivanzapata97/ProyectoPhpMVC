4941d8c42344cc35ee744d6e1e9f197f1fdbcbd2
ivanzapata97 <ivanzapata.f@gmail.com>
2019-06-16 17:53:29 -0300

    nuevas funcionalidades(editar y borrar)

--- controllers/ProductoController.php
+++ controllers/ProductoController.php
@@ -42,25 +42,34 @@ class productoController{
                 $producto->setStock($stock);
 
                 //guardar la imagen
-                $file = $_FILES['imagen'];
-                $filename = $file['name'];
-                $mimetype = $file['type'];
-                
-
-                if($mimetype == "image/jpg" || $mimetype == "image/jpeg" || $mimetype == "image/png" || $mimetype == "image/gif"){
+                if(isset($_FILES['imagen'])){
+                    $file = $_FILES['imagen'];
+                    $filename = $file['name'];
+                    $mimetype = $file['type'];
                     
-                    if(!is_dir('uploads/images')){
-                        mkdir('uploads/images',0777,true);
+
+                    if($mimetype == "image/jpg" || $mimetype == "image/jpeg" || $mimetype == "image/png" || $mimetype == "image/gif"){
+                        
+                        if(!is_dir('uploads/images')){
+                            mkdir('uploads/images',0777,true);
+                        }
+                        $producto->setImagen($filename);
+                        move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
                     }
-                    $producto->setImagen($filename);
-                    move_uploaded_file($file['tmp_name'], 'uploads/images/'.$filename);
                 }
                 else{
                     $_SESSION['producto'] = "failed";
                 }
+                if(isset($_GET['id'])){
+                    $id  = $_GET['id'];
+                    $producto->setId($id);
+                    $save = $producto->edit();
+                }else {
+                    //var_dump($producto); die();
+                    $save = $producto->save();
 
+                }
 
-                $save = $producto->save();
                 if($save){
                     $_SESSION['producto'] = "complete";
                 }
@@ -78,7 +87,20 @@ class productoController{
         header("Location:".base_url."producto/gestion");
     }
     public function editar(){
+        Utils::isAdmin();
+        if(isset($_GET['id'])){
+            $id = $_GET['id'];
+            $edit = true;
+            
+            $producto = new Producto();
+            $producto->setId($id);
+            $pro = $producto->getOne();
 
+            require_once 'views/producto/crear.php';
+        }
+        else{
+            header("Location: ".base_url.'producto/gestion');
+        }
     }
     public function eliminar(){
         Utils::isAdmin();
--- models/producto.php
+++ models/producto.php
@@ -76,10 +76,28 @@ class Producto {
         $productos = $this->db->query("SELECT * FROM productos ORDER BY id DESC");
         return $productos;
     }
+    public function getOne(){
+        $producto = $this->db->query("SELECT * FROM productos WHERE id ={$this->getId()}");
+        return $producto->fetch_object();
+    }
     public function save(){
-        $sql = "INSERT INTO productos VALUES (null,'{$this->getCategoria_id()}','{$this->getNombre()}','{$this->getDescripcion()}',{$this->getPrecio()},{$this->getStock()},null,CURDATE(),'{$this->getImagen()}')";
+        $sql = "INSERT INTO productos VALUES (null,'{$this->getCategoria_id()}','{$this->getNombre()}','{$this->getDescripcion()}',{$this->getPrecio()},{$this->getStock()}, null, CURDATE(), '{$this->getImagen()}')";
         $save = $this->db->query($sql);
 
+        $result = false;
+        if($save){
+            $result = true;
+        }
+        return $result;
+    }
+    public function edit(){
+        $sql = "UPDATE productos SET categoria_id = {$this->getCategoria_id()}, nombre = '{$this->getNombre()}', descripcion = '{$this->getDescripcion()}', precio = {$this->getPrecio()}, stock = {$this->getStock()} ";
+        if($this->getImagen() != null){
+            $sql .= ", imagen = '{$this->getImagen()}'";
+        }
+        $sql .= "WHERE id={$this->id};"; 
+
+        $save = $this->db->query($sql);
         $result = false;
         if($save){
             $result = true;
